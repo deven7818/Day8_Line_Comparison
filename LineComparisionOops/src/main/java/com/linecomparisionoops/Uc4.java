2364d80a2ea1738490948a9d7aa4addfc8cb4b72
deven7818 <deven7818@gmail.com>
2022-02-22 23:43:07 +0530

    Line Comparison Uc4

---
+++ LineComparisionOops/src/main/java/com/linecomparisionoops/Uc4.java
@@ -0,0 +1,72 @@
+package com.linecomparisionoops;
+
+import java.util.Scanner;
+
+public class Uc4 {
+	public static int x1, x2, y1, y2, x3, x4, y3, y4;
+	public static double length1 = lengthCal(x1, x2, y1, y2);
+	public static double length2 = lengthCal(x3, x4, y3, y4);
+	static String s1 = String.valueOf(length1);
+	static String s2 = String.valueOf(length2);
+
+	public static void main(String[] args) {
+		/*
+		 * Taking user input for both points
+		 */
+		System.out.println("Welcome to Line Comparison Computation Program!!");
+		Scanner sc = new Scanner(System.in);
+		/*
+		 * Line 1 values
+		 */
+		System.out.println("Enter points for First Line");
+		
+		System.out.println("Enter Values For First Point");
+		x1 = sc.nextInt();
+		y1 = sc.nextInt();
+
+		System.out.println("Enter Values For Second Point");
+		x2 = sc.nextInt();
+		y2 = sc.nextInt();
+
+		/*
+		 * Line 2 values
+		 */
+		System.out.println("Enter points for Second Line");
+		
+		System.out.println("Enter Values For First Point");
+		x3 = sc.nextInt();
+		y3 = sc.nextInt();
+
+		System.out.println("Enter Values For Second Point");
+		x4 = sc.nextInt();
+		y4 = sc.nextInt();
+		sc.close();
+		/*
+		 * Calling method by creating Object to check equality
+		 */
+		Uc4 compare = new Uc4();
+		compare.lengthComparison(s1, s2);
+	}
+
+	/*
+	 * Method for line length calculation
+	 */
+	public static double lengthCal(int x1, int x2, int y1, int y2) {
+
+		double length = (double) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return length;
+	}
+
+	/*
+	 * Method to check equality in lengths of lines
+	 */
+	public void lengthComparison(String s1, String s2) {
+		if (s1.equals(s2)) {
+			System.out.println("Two line are equal");
+		} else if (s1.compareTo(s2) > 0) {
+			System.out.println("length of line1 is greater than line2");
+		} else {
+			System.out.println("length of line2 is greater than line1");
+		}
+	}
+}
