b18f812fbb9349318b9ac364dff80e3079cfae11
deven7818 <deven7818@gmail.com>
2022-02-22 23:33:26 +0530

    Line Comparison

---
+++ LineComparisionOops/src/main/java/com/linecomparisionoops/App.java
@@ -0,0 +1,9 @@
+package com.linecomparisionoops;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation..!" );
+    }
+}
